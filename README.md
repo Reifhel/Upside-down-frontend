cb7fc0e7025bb229987f1ae9bca459a45531f893
unknown <rafaelherdt1@gmail.com>
2022-08-24 18:24:08 -0300

    css finishing

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 <p align="center">
     <img width="300" src="https://reifhel.github.io/Upside-down-frontend/assets/images/banner/logo.svg">
 </p>
+-------
+## 💻 technologies
+- HTML
+- CSS
+- JavaScript
---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const themeSwitcher = document.getElementById('switch-theme-button')
+themeSwitcher.addEventListener('click', switchTheme);
+
+window.addEventListener('click', function(){
+    const audio = document.getElementById('music');
+    audio.play();
+    audio.volume = 0.2;
+});
+function switchTheme(){
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    const theme = document.body.classList[0];
+
+    let music = theme === 'light-theme' ? 'normal-world.mpeg' : 'inverted-world.mpeg';
+
+    const audio = document.getElementById('music');
+    audio.src = `../assets/musics/${music}`;
+    audio.play();
+    audio.volume = 0.2;
+
+}
